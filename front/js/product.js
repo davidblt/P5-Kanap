23da88966d1d0f90cf8ab79c389934aa5df7a468
David Blot <david.blot9@gmail.com>
2022-08-22 15:08:18 +0200

    product.js : add to cart function

--- front/js/product.js
+++ front/js/product.js
@@ -1,49 +1,61 @@
-/**
- *La variable "url" récupère tous les paramètres de l'URL de la page "product" en cours.
- *URLSearchParams.get, avec la variable "id", récupére l'id de l'URL.
- *requête fetch à l'API pour récupérer les données du produit, puis traitement de la promise brute en JSON.
- *Puis insertion des specifications du produit dans le code HTML.
- */
-const url = window.location.search; // (?id=...)
-const id = new URLSearchParams(url).get('id');
-
-// variables pour insertion des infos produit(s) + ajout panier :
+// variables pour insertion des infos produit + ajout panier :
 const productImg = document.createElement('img');
 const productName = document.getElementById('title');
 const productPrice = document.getElementById('price');
 const productDescription = document.getElementById('description');
 const colorSelect = document.getElementById('colors');
+const productQuantity = document.getElementById('quantity');
 const addToCartBtn = document.getElementById('addToCart');
 
-// requête API pour récupérer les infos du produit identifié par son id :
+/**
+ * La variable "url" récupère tous les paramètres de l'URL de la page "product" * en cours après le ?.
+ * URLSearchParams.get(), récupére la variable id de l'URL.
+ */
+const url = window.location.search; // (?id=...)
+const id = new URLSearchParams(url).get('id');
+
+/**
+ * requête API pour récupérer les infos du produit avec id :
+ * réponse (res) transformée en JSON,
+ * puis nouvelle réponse (data) devient 'product'.
+ * si erreur, insère un message dans le DOM pour informer
+ * 'utilisateur. La console indique aussi une erreur.
+ */
 const apiRequestProduct = async () => {
 	await fetch(`http://localhost:3000/api/products/${id}`)
 		.then((res) => res.json())
 		.then((data) => (product = data))
-		.catch((error) => {
-			productName.style.fontSize = '2rem';
-			productName.textContent =
-				'Désolé, une erreur de chargement est survenue...';
+		.catch((err) => {
+			let error = document.querySelector('.item__content');
+			error.style.fontSize = '1.7rem';
+			error.textContent = 'Désolé, le site est indisponible pour le moment...';
 		});
 };
 
-// fonction qui crée et insère les éléments du produit dans le DOM :
+/**
+ * crée et insère les éléments du produit dans le DOM :
+ * attente de la réponse de l'API avant
+ * insertion (await):
+ * 		- 'title' du document
+ * 		- balise image + attribut 'alt'
+ * 		- nom + prix + description
+ * 		- choix de couleurs : pour chaque couleur du tableau
+ * 	 "colorsTab", créer une option avec l'attribut "color" et y
+ * 	 insèrer le texte de la couleur correspondante.
+ * Puis appel de la fonction pour éxécution.
+ */
 const displayProduct = async () => {
 	await apiRequestProduct();
-	// insertion 'title' du document :
-	document.title = `${product.name}`;
+	document.title = product.name;
 
-	// balise image + attribut "alt" :
 	productImg.src = product.imageUrl;
 	productImg.alt = product.altTxt;
 	document.querySelector('.item__img').appendChild(productImg);
 
-	// nom, prix et description :
 	productName.textContent = product.name;
 	productPrice.textContent = product.price;
 	productDescription.textContent = product.description;
 
-	/* choix de couleurs : pour chaque couleur du tableau "colorsTab", créer une option avec l'attribut "color" et y insèrer le texte de la couleur correspondante, puis les insèrer dans la balise <select> */
 	let colorList = product.colors;
 	for (let color of colorList) {
 		const colorOption = document.createElement('option');
@@ -53,3 +65,67 @@ const displayProduct = async () => {
 	}
 };
 displayProduct();
+
+/**
+ * écoute au clic du bouton "ajouter au panier" :
+ * créer l'objet 'item' contenant les infos produit à stocker.
+ * appeler la fonction qui permet d'ajouter au local storage.
+ */
+addToCartBtn.addEventListener('click', () => {
+	let item = {
+		id: id,
+		name: product.name,
+		color: colorSelect.value,
+		qty: productQuantity.value,
+	};
+	addCart(item);
+});
+
+// stocke le panier (cartArray) dans le local storage en JSON :
+const saveCart = (cartArray) => {
+	localStorage.setItem('added', JSON.stringify(cartArray));
+};
+
+/**
+ * ajout (item) au panier dans le local storage :
+ * 1. récupèrer le panier du local storage traduit en objet.
+ * 2. Vérifier si le panier existe et comprend déjà le même article :
+ * 	a) si le panier n'existe pas (null), créer le tableau, y ajouter les
+ * 	  articles et enregistrer dans le local storage.
+ *	b) si le panier existe vérifier si un article de même id et couleur
+ * 	  y est déjà présent avec .find() :
+ * 		- si undefined = pas de doublon, ajouter l'article dans le
+ * 	  panier et enregistrer dans le local storage.
+ * 		- si déjà présent, incrémenter la nouvelle quantité et
+ *    enregistrer dans le local storage.
+ * 	c) si le panier existe sans article similaire : ajouter l'article
+ * 	  dans le panier et enregistrer dans le local storage.
+ * 3. afficher un message de confirmation au client.
+ */
+const addCart = (item) => {
+	let cartArray = JSON.parse(localStorage.getItem('added'));
+	if (cartArray == null) {
+		cartArray = [];
+		cartArray.push(item);
+		saveCart(cartArray);
+	} else {
+		let foundItem = cartArray.find(
+			(art) => art.id == item.id && art.color == item.color
+		);
+		if (foundItem != undefined) {
+			foundItem.qty = Number(item.qty) + Number(foundItem.qty);
+			saveCart(cartArray);
+		} else {
+			cartArray.push(item);
+			saveCart(cartArray);
+		}
+	}
+	alert(
+		`${item.qty}` +
+			' ' +
+			`${product.name}` +
+			' ' +
+			`${item.color}` +
+			' ajouté(s) à votre panier !'
+	);
+};
